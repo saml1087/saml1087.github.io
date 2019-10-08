0386128c71b1c8357725e2c70e134b08c3d74f02
saml1087 <56265214+saml1087@users.noreply.github.com>
2019-10-08 10:59:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Sam Lee"
+let userName = "saml1087";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
